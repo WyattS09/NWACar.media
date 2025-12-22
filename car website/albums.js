8d0e9e10ea40e4d2c4cfad0c06a035a27a1939b6
WyattS09 <wyatt.schraeder@gmail.com>
2025-12-21 23:03:59 -0600

    Add files via upload

---
+++ car website/albums.js
@@ -0,0 +1,9 @@
+const albums = 
+    {
+        title: "May 14 – Sunset Cruise",
+        date: "2025-05-14",
+        thumbnail: "images/thumbs/may14.jpg",
+        lightroomUrl: "https://adobe.ly/4nG1hKt"
+      }
+      
+  
