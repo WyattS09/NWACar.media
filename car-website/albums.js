4faf287135b3f11f73f958b475f3263dcaab834e
WyattS09 <wyatt.schraeder@gmail.com>
2025-12-22 13:55:53 -0600

    Add files via upload

---
+++ car-website/albums.js
@@ -0,0 +1,30 @@
+const albums = {
+    "dec-21-sunday-shoot": {
+      title: "Dec. 21 Sunday Shoot",
+      path: "images/albums/dec-21-sunday-shoot/",
+      thumbnail: "thumb.jpeg",
+      photos: [
+        "IMG_9979.jpeg",
+        "IMG_9855.jpeg",
+        "IMG_9862.jpeg",
+        "IMG_9856.jpeg",
+        "IMG_9889.jpeg",
+        "IMG_9900.jpeg",
+        "IMG_9925.jpeg",
+        "IMG_9934.jpeg",
+        "IMG_9942.jpeg",
+        "IMG_9943.jpeg",
+        "IMG_9952.jpeg",
+        "IMG_9958.jpeg",
+        "IMG_9964.jpeg",
+        "IMG_9966.jpeg",
+        "IMG_9969.jpeg",
+        "IMG_9971.jpeg",
+        "IMG_9973.jpeg",
+        "thumb.jpeg",
+      ]
+    },
+  };
+  
+      
+  
