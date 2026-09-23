f00e2461d0531594bb7d6c8e123d44fff9c4ff15
WyattS09 <wyatt.schraeder@gmail.com>
2026-09-23 13:35:39 -0500

    our cars coded just not implemented

---
+++ car-website/cars.js
@@ -0,0 +1,40 @@
+const cars = {
+
+    /*
+    "example-car": {
+      title: "2018 Subaru WRX",
+      year: "2018",
+      path: "images/cars/wrx/",
+      thumbnail: "thumbnail.jpg",
+  
+      completed: "Lowering springs, wheels and tires, window tint, and a cat-back exhaust.",
+      planned: "Front lip, paint correction, upgraded brakes, and a future tune.",
+  
+      photos: [
+        "photo-1.jpg",
+        "photo-2.jpg",
+        "photo-3.jpg"
+      ]
+    },
+  */
+
+    "Wills car": {
+      title: "2001 Mustang GT",
+      year: "2001",
+      path: "images/cars/wills-stang/",
+      thumbnail: "thumb.JPG",
+  
+      completed: "",
+      planned: "",
+  
+      photos: [
+        "IMG_0685.JPG",
+        "IMG_0686.JPG",
+        "IMG_0687.JPG",
+        "IMG_0688.JPG",
+        "IMG_0689.JPG",
+        "IMG_0690.JPG",
+        "thumb.JPG",
+      ]
+    }
+  };
